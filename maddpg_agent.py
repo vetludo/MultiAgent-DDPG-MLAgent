e162de0b186cdd9e2dc1df769957cfd02cf5749c
vetludo <70711469+vetludo@users.noreply.github.com>
2020-09-21 21:46:18 +0800

    Update maddpg_agent.py

--- maddpg_agent.py
+++ maddpg_agent.py
@@ -1,10 +1,3 @@
-"""
-This is the multiagent class for handling two DDPG agent for each racket. 
-Each agent is using DDPG and only input own observations to actor network and learn all states and actions of both agent in critic network.
-I have implemented replay buffer memory initialization, n_step rollout and annealing epilson policy.
-A replay buffer class is implemented for storing experiences for agents to learn.
-"""
-
 import torch
 import torch.optim as optim
 import numpy as np
@@ -12,7 +5,10 @@ from collections import deque
 import random
 from .model import Actor, Critic
 
-
+"""
+This is the multiagent class for handling two DDPG agent for each racket.
+I have implemented replay buffer memory initialization, n_step rollout and annealing epilson policy.
+"""
 class MADDPG_Net:
     def __init__(self, env, args):
         np.random.seed(args.random_seed)        
@@ -114,7 +110,9 @@ class MADDPG_Net:
         n = np.random.normal(0, 1, shape)
         return self.e * n
 
-
+"""
+Each agent is using DDPG and only input own observations to actor network and learn all states and actions of both agent in critic network.
+"""
 class DDPG_Agent:
     def __init__(self, state_size, action_size, args, agent_count=1, l2_decay=0.0001):
         self.device = args.device
@@ -183,7 +181,9 @@ class DDPG_Agent:
             projected_probs[idx].index_add_(0, upper_bound[idx].long(), m_upper[idx].double())
         return projected_probs.float()
 
-
+"""
+A replay buffer class is implemented for storing experiences for agents to learn.
+"""
 class ReplayBuffer:
     def __init__(self, device, buffer_size=100000, gamma=0.99, rollout=5, agent_count=1):
         self.buffer = deque(maxlen=buffer_size)
