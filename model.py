34a3fc66d6905388cf3ffff52801f0797d5cb959
vetludo <70711469+vetludo@users.noreply.github.com>
2020-09-21 21:42:09 +0800

    Update model.py

--- model.py
+++ model.py
@@ -3,10 +3,15 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-WEIGHT_LOW = -3e-2
-WEIGHT_HIGH = 3e-2
+
+def initialize_weights(net, low=-3e-2, high=3e-2):
+    for param in net.parameters():
+        param.data.uniform_(low, high)
 
 
+"""
+Actor Network
+"""
 class Actor(nn.Module):
     def __init__(self, state_size, action_size, seed, fc1_units=400, fc2_units=300):
         super(Actor, self).__init__()
@@ -14,32 +19,23 @@ class Actor(nn.Module):
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        self.fc1.weight.data.uniform_(WEIGHT_LOW, WEIGHT_HIGH)
-        self.fc2.weight.data.uniform_(WEIGHT_LOW, WEIGHT_HIGH)
-        self.fc3.weight.data.uniform_(WEIGHT_LOW, WEIGHT_HIGH)
+        initialize_weights(self)
 
     def forward(self, state):
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
         return self.fc3(x).tanh()
 
-
+"""
+Critic Network
+"""
 class Critic(nn.Module):
     def __init__(self, state_size, action_size, num_atoms, seed, fcs1_units=400, fc2_units=300):
         super(Critic, self).__init__()
-        self.seed = torch.manual_seed(seed)
         self.fcs1 = nn.Linear(state_size, fcs1_units)
         self.fc2 = nn.Linear(fcs1_units + action_size, fc2_units)
         self.fc3 = nn.Linear(fc2_units, num_atoms)
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        self.fcs1.weight.data.uniform_(WEIGHT_LOW, WEIGHT_HIGH)
-        self.fc2.weight.data.uniform_(WEIGHT_LOW, WEIGHT_HIGH)
-        self.fc3.weight.data.uniform_(WEIGHT_LOW, WEIGHT_HIGH)
+        initialize_weights(self)
 
     def forward(self, state, action, log=False):
         xs = F.relu(self.fcs1(state))
